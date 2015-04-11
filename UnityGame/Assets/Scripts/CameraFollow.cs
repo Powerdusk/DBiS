5de6404c4c0a71927e8e47db3d128f90af656dd7
9-ar7k <aj@obsoletebrowser.net>
2015-04-11 14:00:22 -0700

    Trying Camera Follow stuff

--- UnityGame/Assets/Scripts/CameraFollow.cs
+++ UnityGame/Assets/Scripts/CameraFollow.cs
@@ -15,12 +15,11 @@ public class CameraFollow : MonoBehaviour {
 	void Update()
 	{
 		float mDist = Mathf.Sign((transform.position.x - target.position.x)* -1);
-		thisTrans = new Vector2(mDist * speed,0);
 
-		Debug.Log(mDist);
-		Debug.Log(thisTrans);
-
-		if ( Mathf.Abs(transform.position.x - target.position.x) > 2)
-		transform.Translate((0.08f * mDist),0,0);
+		float mPad = 2*mDist;
+		//thisTrans = new Vector2(mDist * speed,0);
+		//if ( Mathf.Abs(transform.position.x - target.position.x) > 2)
+			transform.position = Vector3.Lerp (transform.position, target.position, speed) + new Vector3(mPad,0,-10);
+		//transform.Translate((0.08f * mDist),0,0);
 	}
 }
