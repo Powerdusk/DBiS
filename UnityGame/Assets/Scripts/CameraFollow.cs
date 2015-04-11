5e8a0722e23a21988d3cb4eac6c783171c7d205b
9-ar7k <aj@obsoletebrowser.net>
2015-04-11 16:30:48 -0700

    Camera Stuff Broken will fix later

--- UnityGame/Assets/Scripts/CameraFollow.cs
+++ UnityGame/Assets/Scripts/CameraFollow.cs
@@ -17,8 +17,9 @@ public class CameraFollow : MonoBehaviour {
 		float mDist = Mathf.Sign((transform.position.x - target.position.x)* -1);
 
 		float mPad = 2*mDist;
+
 		//thisTrans = new Vector2(mDist * speed,0);
-		//if ( Mathf.Abs(transform.position.x - target.position.x) > 2)
+		if ( Mathf.Abs(transform.position.x - target.position.x) > 0)
 			transform.position = Vector3.Lerp (transform.position, target.position, speed) + new Vector3(mPad,0,-10);
 		//transform.Translate((0.08f * mDist),0,0);
 	}
