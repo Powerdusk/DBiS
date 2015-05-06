8a8df182cea0753184263a030b0ae172b67cf050
9-ar7k <aj@obsoletebrowser.net>
2015-05-06 11:48:23 -0700

    the beginning...again
    start over

---
+++ UnityGame/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float distance;
+	public float height;
+	public float width;
+	public float damping;
+
+	void Update()
+	{
+		Vector3 wantedPosition = target.TransformPoint(width,height, - distance);
+		transform.position = Vector3.Lerp(transform.position, wantedPosition,Time.deltaTime * damping);
+	}
+}
