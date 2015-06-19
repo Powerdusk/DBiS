accf11e660872f26f17a2587d61b1d71e0f71e32
TommyTran93 <TommyTran1993@gmail.com>
2015-06-18 17:48:03 -0700

    update to 5.1.1, also made changes to util.cs
    if util.cs causes anymore errors tell me

--- UnityGame/Assets/UniMerge/Editor/Util.cs
+++ UnityGame/Assets/UniMerge/Editor/Util.cs
@@ -221,9 +221,9 @@ namespace UniMerge {
 							yield return mine.quaternionValue.Equals(theirs.quaternionValue);
 							yield break;
 #elif !(UNITY_3_0 || UNITY_3_0_0 || UNITY_3_1 || UNITY_3_2 || UNITY_3_3 || UNITY_3_4 || UNITY_3_5)
-						case (SerializedPropertyType)16:
+						/*case (SerializedPropertyType)16:
 							yield return mine.quaternionValue.Equals(theirs.quaternionValue);
-							yield break;
+							yield break;*/
 #endif
 							default:
 							Debug.LogWarning("Unknown SeralizedPropertyType encountered: " + mine.propertyType);
@@ -329,9 +329,9 @@ namespace UniMerge {
 					to.quaternionValue = from.quaternionValue;
 					break;
 #elif !(UNITY_3_0 || UNITY_3_0_0 || UNITY_3_1 || UNITY_3_2 || UNITY_3_3 || UNITY_3_4 || UNITY_3_5)
-				case (SerializedPropertyType)16:			
+/*				case (SerializedPropertyType)16:			
 					to.quaternionValue = from.quaternionValue;
-					break;
+					break;*/
 #endif
 				default:
 					Debug.LogWarning("Unknown SeralizedPropertyType encountered: " + to.propertyType);
