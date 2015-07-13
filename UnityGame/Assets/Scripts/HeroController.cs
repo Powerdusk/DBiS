c14e2af8b5185680e2a48ade9b7fa6342345dda3
Christopher Peterson <cjpeter1@uci.edu>
2015-07-12 20:34:49 -0700

    Stuff?

--- UnityGame/Assets/Scripts/HeroController.cs
+++ UnityGame/Assets/Scripts/HeroController.cs
@@ -41,7 +41,7 @@ public class HeroController : MonoBehaviour {
 	void FixedUpdate()
 	{
 		
-		//GROUNDED?
+		float move = Input.GetAxisRaw("Horizontal");
 		grounded = Physics2D.OverlapCircle(groundCheck.position,groundRadius,isGround);
 		anim.SetBool("Ground", grounded);
 		if(grounded && !walled)
@@ -61,16 +61,20 @@ public class HeroController : MonoBehaviour {
 			gameObject.GetComponent<Rigidbody2D>().gravityScale = .3f;
 		else
 			gameObject.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
-		//print (grounded);
-		
-		//anim.SetFloat ("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
-		
-		float move = Input.GetAxisRaw("Horizontal");
+
 
 		if(!walled)
 			GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		else{
+			if(ledge){
+				GetComponent<Rigidbody2D>().velocity = new Vector2(0f,0f);
+				gameObject.GetComponent<Rigidbody2D>().gravityScale = 0f;
+			}
+			else
+				gameObject.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
+		}
 
-		//anim
+		
 		anim.SetFloat("speed", Mathf.Abs(move));
 		//avoid constantly referenceing input
 		float Hinput = Input.GetAxisRaw ("Horizontal");
@@ -79,15 +83,15 @@ public class HeroController : MonoBehaviour {
 		{
 			GetComponent<Transform>().localScale = new Vector3 (Mathf.Sign (Hinput),1,1);
 		}
+
+
+		
 		if(Input.GetButtonUp("Jump"))
 			jumpNum++;
 		//if you are grounded, pressing the button and have not already started a jump
 		if (jumpNum < jumpMax && Input.GetButtonDown ("Jump") && !isPrejump) {
-			//start anim
 			anim.SetBool ("preJump", true);
-			//start a jump
 			isPrejump = true;
-			//reset frame counter
 			preJumpCount = 0;
 		} else if(grounded && Input.GetButton("Jump") && isPrejump) {
 			//increment frame counter
